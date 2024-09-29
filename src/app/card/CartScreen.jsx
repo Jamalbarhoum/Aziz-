4e313dccb82542fed3c23d868bdcab364bf375ef
Jamalbarhoum <jamallbarhoum@gmail.com>
2024-09-29 13:39:54 +0300

    card Payment front-end

--- src/app/card/CartScreen.jsx
+++ src/app/card/CartScreen.jsx
@@ -9,9 +9,154 @@ import {
   Text,
   Button,
   ScrollView,
+  TextInput,
 } from "react-native";
 import { CartContext } from "../CartProvider";
 import Icon from "react-native-vector-icons/FontAwesome";
+
+// import React from 'react';
+// import { Button } from 'react-native';
+import { Payment } from "react-native-payments";
+
+const PaymentForm = () => {
+  const [name, setName] = useState("");
+  const [cardNumber, setCardNumber] = useState("");
+  const [expiryDate, setExpiryDate] = useState("");
+  const [cvv, setCvv] = useState("");
+  const [toggle, setToggle] = useState(false);
+
+  const [cashButtonColor, setCashButtonColor] = useState("#3E9FC0");
+  const [cashButtonColor2, setCashButtonColor2] = useState("#3E9FC0");
+  const handlePayment = () => {
+    // هنا يمكنك معالجة الدفع باستخدام معلومات النموذج
+    console.log({
+      name,
+      cardNumber,
+      expiryDate,
+      cvv,
+    });
+  };
+
+  return (
+    <>
+      <View style={stylesF.headerContainer}>
+        <Text style={styles.txt}>Payment methods</Text>
+        <View style={stylesF.buttonContainer}>
+          {toggle && (
+            <Button
+              title="Cancel"
+              onPress={() => {
+                setToggle(!toggle);
+              }}
+            />
+          )}
+        </View>
+      </View>
+      <View style={stylesF.container}>
+        <View>
+          {!toggle && (
+            <Button
+              title="Payment"
+              color={cashButtonColor2}
+              onPress={() => {
+                setToggle(!toggle);
+              }}
+            />
+          )}
+
+          {!toggle && (
+            <Button
+              title="Cash"
+              color={cashButtonColor}
+              onPress={() => {
+                setCashButtonColor("green");
+                setCashButtonColor2("#3E9FC0");
+              }}
+            />
+          )}
+        </View>
+        {toggle && (
+          <ScrollView>
+            <View>
+              <Text style={stylesF.label}>Cardholder's Name</Text>
+              <TextInput
+                style={stylesF.input}
+                value={name}
+                onChangeText={setName}
+                placeholder="Enter your name"
+              />
+
+              <Text style={stylesF.label}>Card Number</Text>
+              <TextInput
+                style={stylesF.input}
+                value={cardNumber}
+                onChangeText={setCardNumber}
+                placeholder="Enter card number"
+                keyboardType="numeric"
+              />
+
+              <Text style={stylesF.label}>Expiry Date (MM/YY)</Text>
+              <TextInput
+                style={stylesF.input}
+                value={expiryDate}
+                onChangeText={setExpiryDate}
+                placeholder="Enter expiry date"
+              />
+
+              <Text style={stylesF.label}>CVV</Text>
+              <TextInput
+                style={stylesF.input}
+                value={cvv}
+                onChangeText={setCvv}
+                placeholder="Enter CVV"
+                keyboardType="numeric"
+              />
+
+              <Button
+                title="Proceed to Payment"
+                onPress={() => {
+                  handlePayment();
+                  setToggle(!toggle);
+                  setCashButtonColor("#3E9FC0");
+                  setCashButtonColor2("green");
+                }}
+              />
+            </View>
+          </ScrollView>
+        )}
+      </View>
+    </>
+  );
+};
+
+const stylesF = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    height: "100%",
+  },
+  label: {
+    marginVertical: 5,
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 15,
+  },
+  headerContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginVertical: 10,
+    marginHorizontal: 10,
+  },
+});
 
 function CartScreen() {
   const { cartItems, setCartItems } = useContext(CartContext);
@@ -63,14 +208,21 @@ function CartScreen() {
       <Image source={{ uri: item.imageUrl }} style={styles.itemImage} />
       <View style={styles.quantityContainer}>
         <Text style={styles.quantityText}>x{item.quantity}</Text>
-        
       </View>
-      <View style={{direction:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}}>
-      <Text style={styles.priceText}><Text style={{color:"#0129",fontWeight:"normal"}}>JD</Text> {item.price}</Text>
-      <View style={styles.line}></View>
+      <View
+        style={{
+          direction: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text style={styles.priceText}>
+          <Text style={{ color: "#0129", fontWeight: "normal" }}>JD</Text>{" "}
+          {item.price}
+        </Text>
+        <View style={styles.line}></View>
       </View>
-   
-      
     </TouchableOpacity>
   );
 
@@ -90,7 +242,7 @@ function CartScreen() {
           />
         )}
         {cartItems.length !== 0 && (
-          <Button title="Edit Cart" onPress={handleOpenEditModal} />
+          <Button color={"#3E9FC0"}  title="Edit Cart" onPress={handleOpenEditModal} />
         )}
       </View>
       {/* Modal for editing items */}
@@ -110,25 +262,40 @@ function CartScreen() {
                       {item.description}
                     </Text>
                     <View style={styles.modalButtonContainer}>
-          <Icon  onPress={() => handleDecreaseQuantity(item.id)} name="minus" size={15} color="black" />
-                  
+                      <Icon
+                        onPress={() => handleDecreaseQuantity(item.id)}
+                        name="minus"
+                        size={15}
+                        color="black"
+                      />
+
                       <Text style={styles.modalQuantityText}>
                         {item.quantity}
                       </Text>
-                    
-          
-          <Icon   onPress={() => handleIncreaseQuantity(item.id)} name="plus" size={15} color="black" />
-                      <Icon onPress={() => handleRemoveItem(item.id)}  name="trash" size={22} color="red" />
+
+                      <Icon
+                        onPress={() => handleIncreaseQuantity(item.id)}
+                        name="plus"
+                        size={15}
+                        color="black"
+                      />
+                      <Icon
+                        onPress={() => handleRemoveItem(item.id)}
+                        name="trash"
+                        size={22}
+                        color="red"
+                      />
                     </View>
                   </View>
                 </View>
               ))}
             </ScrollView>
-            <Button title="Save Changes" onPress={handleSaveChanges} />
-            <Button title="Cancel" onPress={handleCloseModal} />
+            <Button color={"#3E9FC0"} title="Save Changes" onPress={handleSaveChanges} />
+            <Button color={"#3E9FC0"}  title="Cancel" onPress={handleCloseModal} />
           </View>
         </Modal>
       )}
+      {cartItems.length > 0 && <PaymentForm />}
     </View>
   );
 }
@@ -139,9 +306,14 @@ const styles = StyleSheet.create({
     paddingTop: 20,
     paddingBottom: 20,
     backgroundColor: "#fff",
+    height:"100%",
+    zIndex:1,
+    position:"static"
+
   },
   containerViewCard: {
-    flex: 0.35,
+    flex: 1,
+   
   },
   title: {
     fontSize: 24,
@@ -265,9 +437,14 @@ const styles = StyleSheet.create({
   },
   priceText: {
     fontSize: 16, // حجم الخط
-    color: '#000', // لون الخط (يمكن تغييره حسب الحاجة)
+    color: "#000", // لون الخط (يمكن تغييره حسب الحاجة)
     marginTop: 5, // مسافة فوق السعر
-    fontWeight: 'bold', // جعل السعر عريضًا
+    fontWeight: "bold", // جعل السعر عريضًا
+  },
+  txt: {
+    color: "#000",
+    fontSize: 22,
+    padding: 3,
   },
 });
 
